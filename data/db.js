9c3d39ad350f98e348fd14273199941e0a14cc80
Lucas Yepez <lucasyepez99@gmail.com>
2024-11-08 23:34:49 -0600

    edited db

--- data/db.js
+++ data/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://lucasyepez:PUfcwvaonLIPL1sF@portfolio-site-backend.modbl.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-});
+const connectDB = async() => {
+    try {
+        await mongoose.connect('mongodb+srv://lucasyepez:PUfcwvaonLIPL1sF@portfolio-site-backend.modbl.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+module.exports = connectDB;
