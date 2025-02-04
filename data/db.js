36b57fd04b0c3244b2f3b2b46d3f7aa7542b2968
Lucas Yepez <lucasyepez99@gmail.com>
2025-02-03 22:40:44 -0600

    replace mongo url

--- data/db.js
+++ data/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async() => {
     try {
-        await mongoose.connect('mongodb+srv://lucas:kFdIPFU3USLfbqQZ@clust(er0.jsvxv.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://lucas:kFdIPFU3USLfbqQZ@cluster0.jsvxv.mongodb.net/?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
